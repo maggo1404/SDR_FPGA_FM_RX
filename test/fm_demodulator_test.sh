f64d059367f193a2c6ce48eccd75815c3fe3a3ec
Marco <marco_schwan@mailbox.org>
2023-01-18 19:54:58 +0100

    Update and rename src/fm_demodulator_test.sh to test/fm_demodulator_test.sh

--- test/fm_demodulator_test.sh
+++ test/fm_demodulator_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 begin=$(date +%s)
 # echo "Start bei $begin"
-ghdl -a fm_demodulator.vhd
-ghdl -a fm_demodulator_tb.vhd 
+ghdl -a ../src/fm_demodulator.vhd
+ghdl -a ../src/fm_demodulator_tb.vhd 
 ghdl -e fm_demod_tb
 echo "start sim"
 ghdl -r fm_demod_tb --stop-time=20us --wave=fm_demod.ghw
